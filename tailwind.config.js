067b99e1df3d4404f73a5da4dfad9fb64843a909
HasithaDi <dilshanhasitha254@gmail.com>
2024-07-22 21:31:28 +0530

    update

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const { Poppins } = require('next/font/google');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ['poppins', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
